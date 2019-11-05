3dcc611970d1ec48796c73b40dd945be3c152f01
Hans Foster <fosterhans@gmail.com>
2019-11-05 01:45:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   end
   
   def self.find_or_create_by_name(new_name)
-    if find_by_name(new_name) == new_by_name(new_name)
+    if find_by_name(new_name).name == new_name
       return find_by_name(new_name)
     else 
       create_by_name(new_name)
